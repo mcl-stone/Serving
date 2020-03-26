b82613665cd56a860d8dfbcba00a8f8c33508484
MRXLT <xlt2024@gmail.com>
2020-03-26 07:58:57 +0000

    add upload whl

--- compile_and_upload.sh
+++ compile_and_upload.sh
@@ -3,6 +3,10 @@ set -v
 
 version=0.1.4
 
+cd ./python
+python change_version.py $version
+cd ..
+
 export PYTHONROOT=/usr/
 
 git fetch upstream
@@ -45,16 +49,34 @@ tar -czvf serving-gpu-$version.tar.gz serving-gpu-$version/
 cd ..
 }
 
+function cp_whl(){
+cp ./python/dist/paddle_serving_*-$version-py* ../whl_package
+}
+
 function compile_cpu(){
 mkdir -p build_server
 cd build_server
 WITHAVX=$1
 WITHMKL=$2
-cmake -DPYTHON_INCLUDE_DIR=$PYTHONROOT/include/python2.7/ -DPYTHON_LIBRARY=$PYTHONROOT/lib64/libpython2.7.so -DPYTHON_EXECUTABLE=$PYTHONROOT/bin/python2.7 -DWITH_AVX=$WITHAVX -DWITH_MKL=$WITHMKL -DSEVER=ON .. > compile_log
+cmake -DPYTHON_INCLUDE_DIR=$PYTHONROOT/include/python2.7/ -DPYTHON_LIBRARY=$PYTHONROOT/lib64/libpython2.7.so -DPYTHON_EXECUTABLE=$PYTHONROOT/bin/python2.7 -DWITH_AVX=$WITHAVX -DWITH_MKL=$WITHMKL -DSERVER=ON .. > compile_log
 make -j20 >> compile_log
 #make install >> compile_log
+cp_whl
 cd ..
 pack $WITHAVX $WITHMKL
+}
+
+function compile_cpu_py3(){
+mkdir -p build_server_py3
+cd build_server_py3
+WITHAVX=$1
+WITHMKL=$2
+cmake -DPYTHON_INCLUDE_DIR=$PYTHONROOT/include/python3.6m/ -DPYTHON_LIBRARY=$PYTHONROOT/lib64/libpython3.6m.so -DPYTHON_EXECUTABLE=$PYTHONROOT/bin/python3.6m -DWITH_AVX=$WITHAVX -DWITH_MKL=$WITHMKL -DSERVER=ON .. > compile_log
+make -j20 >> compile_log
+#make install >> compile_log
+cp_whl
+cd ..
+#pack $WITHAVX $WITHMKL
 }
 
 function compile_gpu(){
@@ -63,8 +85,21 @@ cd build_gpu_server
 cmake -DPYTHON_INCLUDE_DIR=$PYTHONROOT/include/python2.7/ -DPYTHON_LIBRARY=$PYTHONROOT/lib64/libpython2.7.so -DPYTHON_EXECUTABLE=$PYTHONROOT/bin/python2.7 -DWITH_GPU=ON -DSERVER=ON .. > compile_log
 make -j20 >> compile_log
 #make install >> compile_log
+cp_whl
 cd ..
 pack_gpu
+}
+
+
+function compile_gpu_py3(){
+mkdir -p build_gpu_server_py3
+cd build_gpu_server_py3
+cmake -DPYTHON_INCLUDE_DIR=$PYTHONROOT/include/python3.6m/ -DPYTHON_LIBRARY=$PYTHONROOT/lib64/libpython3.6m.so -DPYTHON_EXECUTABLE=$PYTHONROOT/bin/python3.6m -DWITH_GPU=ON -DSERVER=ON .. > compile_log
+make -j20 >> compile_log
+#make install >> compile_log
+cp_whl
+cd ..
+#pack_gpu
 }
 
 function compile_client(){
@@ -73,6 +108,18 @@ cd build_client
 cmake -DPYTHON_INCLUDE_DIR=$PYTHONROOT/include/python2.7/ -DPYTHON_LIBRARY=$PYTHONROOT/lib64/libpython2.7.so -DPYTHON_EXECUTABLE=$PYTHONROOT/bin/python2.7 -DCLIENT=ON -DPACK=ON .. > compile_log
 make -j20 >> compile_log
 #make install >> compile_log
+cp_whl
+cd ..
+}
+
+
+function compile_client_py3(){
+mkdir -p build_client_py3
+cd build_client_py3
+cmake -DPYTHON_INCLUDE_DIR=$PYTHONROOT/include/python3.6m/ -DPYTHON_LIBRARY=$PYTHONROOT/lib64/libpython3.6m.so -DPYTHON_EXECUTABLE=$PYTHONROOT/bin/python3.6m -DCLIENT=ON -DPACK=ON .. > compile_log
+make -j20 >> compile_log
+#make install >> compile_log
+cp_whl
 cd ..
 }
 
@@ -82,17 +129,20 @@ cd build_app
 cmake -DPYTHON_INCLUDE_DIR=$PYTHONROOT/include/python2.7/ -DPYTHON_LIBRARIES=$PYTHONROOT/lib64/libpython2.7.so -DPYTHON_EXECUTABLE=$PYTHONROOT/bin/python -DAPP=ON .. > compile_log
 make -j20 >> compile_log
 #make install >> compile_log
+cp_whl
 cd ..
 }
 
-function compile_app(){
-mkdir -p build_app
-cd build_app
-cmake -DPYTHON_INCLUDE_DIR=$PYTHONROOT/include/python2.7/ -DPYTHON_LIBRARY=$PYTHONROOT/lib64/libpython2.7.so -DPYTHON_EXECUTABLE=$PYTHONROOT/bin/python2.7 -DAPP=ON ..> compile_log
+function compile_app_py3(){
+mkdir -p build_app_py3
+cd build_app_py3
+cmake -DPYTHON_INCLUDE_DIR=$PYTHONROOT/include/python3.6m/ -DPYTHON_LIBRARY=$PYTHONROOT/lib64/libpython3.6m.so -DPYTHON_EXECUTABLE=$PYTHONROOT/bin/python3.6m -DAPP=ON ..> compile_log
 make -j20 >> compile_log
 #make install >> compile_log
+cp_whl
 cd ..
 }
+
 function upload_bin(){
     cd bin_package
     python ../bos_conf/upload.py serving-cpu-avx-openblas-$version.tar.gz
@@ -102,8 +152,20 @@ function upload_bin(){
     cd ..
 }
 
+function upload_whl(){
+    cd whl_package
+    python ../bos_conf/upload_whl.py paddle_serving_client-$version-py2-none-any.whl 
+    python ../bos_conf/upload_whl.py paddle_serving_server-$version-py2-none-any.whl  
+    python ../bos_conf/upload_whl.py paddle_serving_server_gpu-$version-py2-none-any.whl
+    python ../bos_conf/upload_whl.py paddle_serving_client-$version-py3-none-any.whl 
+    python ../bos_conf/upload_whl.py paddle_serving_server-$version-py3-none-any.whl  
+    python ../bos_conf/upload_whl.py paddle_serving_server_gpu-$version-py3-none-any.whl
+    cd .. 
+}
+
 #cpu-avx-openblas $1-avx  $2-mkl
 #compile_cpu ON OFF
+#compile_cpu_py3 ON OFF
 
 #cpu-avx-mkl
 #compile_cpu ON ON
@@ -113,12 +175,17 @@ function upload_bin(){
 
 #gpu
 #compile_gpu
+#compile_gpu_py3
 
 #client
 #compile_client
+#compile_client_py3
 
 #app
 #compile_app
 
 #upload bin
 #upload_bin
+
+#upload whl
+#upload_whl
